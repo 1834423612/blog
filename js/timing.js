53e5c96fd7e7e3b0187081a610909d3f44cd99a6
kjch666 <1834423612@qq.com>
2022-03-28 01:07:24 +0800

    Site updated: 2022-03-28 01:07:24

--- js/timing.js
+++ js/timing.js
@@ -1,6 +1,6 @@
 let message = document.getElementsByTagName("timing")[0];
 message.style.color = "red"; //设置颜色
-var endtime = new Date("2022/6/7 09:00:00"); //设置目标日期
+var endtime = new Date("2022/6/24 08:00:00"); //设置目标日期
 function showTime(){
 	var stime = new Date();
 	var time = endtime - stime;
@@ -9,7 +9,7 @@ function showTime(){
 		var hour = Math.floor(time%(3600*24*1000)/1000/3600);
 		var min = Math.floor(time%(3600*24*1000)/1000%3600/60);
 		var sec = Math.floor(time%(3600*24*1000)/1000%3600%60);
-		var str ='距离2022年高考还有'+day+'天'+hour+'小时'+min+'分钟'+sec+'秒'; //设置字符串
+		var str ='距离2022年中考还有'+day+'天'+hour+'小时'+min+'分钟'+sec+'秒'; //设置字符串
 		message.innerHTML = str;
 	}
 }
