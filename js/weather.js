bbcac55001bbcf95fe63a27b324a07229878fcb4
kjch666 <1834423612@qq.com>
2022-04-05 00:04:58 +0800

    Site updated: 2022-04-05 00:04:58

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "0214",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "12",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "12",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "ced1072c3a9148ca87775d80b209e71a"
+  }
+}
